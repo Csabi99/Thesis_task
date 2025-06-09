1304b774501f0bc914378336e8e5ffe2d751f867
Csaba Czuth <74929977+Csabi99@users.noreply.github.com>
2025-06-09 11:45:52 +0200

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the argument number from the environment variable
+CLIENT_NUM=${CLIENT_NUM:-1}  # Default to 1 if not set
+
+# Check if NOISE environment variable exists and set the additional parameter
+if [ -n "$NOISE" ]; then
+    # Run the client script with the additional noise parameter
+    python client_carla.py --num=$CLIENT_NUM --noise
+else
+    # Run the client script without the noise parameter
+    python client_carla.py --num=$CLIENT_NUM
+fi
