fbcd7cc3ff1b1aad5f12a138e5dbd5c86e158869
Csaba Czuth <74929977+Csabi99@users.noreply.github.com>
2025-06-09 12:01:40 +0200

    Add files via upload

---
+++ strategy_configs/FedCM/job_configs/generator.sh
@@ -0,0 +1,30 @@
+for i in {1..32}
+do
+cat <<EOF > client-job-$i.yaml
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: client-job-$i
+spec:
+  template:
+    metadata:
+      labels:
+        app: client-$i
+    spec:
+      containers:
+      - name: client
+        image: ccsaba99/client-image:latest  # Replace with your Docker image
+        env:
+        - name: CLIENT_NUM
+          value: "$i"  # Dynamically set CLIENT_NUM based on the iteration
+        volumeMounts:
+        - mountPath: "/app/logs"  # Path inside the container where the volume will be mounted
+          name: storage-volume          
+      restartPolicy: Never  # Ensures the Job does not restart after completion
+      volumes:
+      - name: storage-volume
+        persistentVolumeClaim:
+          claimName: my-nfs-pvc  # Name of the PVC defined above      
+  backoffLimit: 0  # Prevent retries on failure
+EOF
+done
